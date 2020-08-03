ccbbcc7e3d921322cc24a027aee6cddf359ad485
Mike Milewski <mike.milewski@loop.colum.edu>
2020-08-02 20:32:10 -0500

    Level 1

---
+++ Assets/Scripts/TerrainDrawDistance.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TerrainDrawDistance : MonoBehaviour
+{
+    [SerializeField]
+    private Terrain m_terrain;  //reference to your terrain
+
+    [SerializeField]
+    private float DrawDistance; // how far you want to be able to see the grass
+
+    void Start()
+    {
+        m_terrain.detailObjectDistance = DrawDistance;
+    }
+}
