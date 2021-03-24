078de68b283540e8df023d6023d1980e5db97f0e
Mike Milewski <mikemilewski3@gmail.com>
2021-03-24 17:02:53 -0500

    Audio Fixes
    Fixed an issue where some audio queues would still play after the volume toggle in the settings menu has been ticked off.

---
+++ Assets/Scripts/ToggleAudio.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable 0649
+using UnityEngine;
+
+public class ToggleAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private Settings settings;
+
+    private void OnEnable()
+    {
+        if(!settings.MuteAudio)
+        {
+            audioSource.volume = 1;
+        }
+        else
+        {
+            audioSource.volume = 0;
+        }
+    }
+}
